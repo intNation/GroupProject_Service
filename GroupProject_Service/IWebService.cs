81c43a46be864ce6e09c332db009ba5eb6aae145
Nation <mandlanation3@gmail.com>
2024-09-26 10:58:03 +0200

    Add project files.

---
+++ GroupProject_Service/IWebService.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GroupProject_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWebService" in both code and config file together.
+    [ServiceContract]
+    public interface IWebService
+    {
+
+        [OperationContract]
+        bool Register(User user);
+
+        [OperationContract]
+        User Login(string logype, string password);
+
+        [OperationContract]
+        bool userExistence(string email);
+
+        [OperationContract]
+        List<Product> GetProducts();
+
+        [OperationContract]
+        List<Product> GetProductsOnSale();
+
+        [OperationContract]
+        List<Product> GetProductsOnSaleOnCategory(string category);
+
+        [OperationContract]
+        List<Product> GetFeaturedProducts();
+
+
+        [OperationContract]
+        Product GetProductByID(int id);
+
+        [OperationContract]
+        List<Product> GetProductsbyCategory(string category);
+
+        [OperationContract]
+        List<string> GetCategories();
+
+        [OperationContract]
+        Product GetProductbyName(string prod_name);
+
+        [OperationContract]
+        void AddToWishList(int productId,int wishListId);
+
+        [OperationContract]
+        void AddToCart(int productId, int cartID);
+
+        [OperationContract]
+        List<Product> GetWishListItems(int wishlistId);
+
+        [OperationContract]
+        Cart GetCart(int session);
+
+        [OperationContract]
+        List<Product> GetCartItems(int cartId);
+        //
+        [OperationContract]
+        bool increaseQuantity(int id);
+        //
+        [OperationContract]
+        bool decreaseQuantity(int id);
+        //                          
+        [OperationContract]
+        bool RemoveFromWishlist(int id);
+        //
+        [OperationContract]
+        bool RemoveFromCart(int id);
+        //
+        [OperationContract]
+        bool AddProduct(int productId);
+       
+        [OperationContract]
+        bool EditProduct(int productId);
+
+        [OperationContract]
+        bool DeleteProduct(int productId);
+
+        [OperationContract]
+        double GetTotalPrice();
+
+        [OperationContract]
+        int PlaceOrder(Order order);
+
+        [OperationContract]
+        void GenerateInvoice(Invoice invoice);
+
+        [OperationContract]
+        List<Invoice> GetSortedInvoices(int userId, string orderBy);
+
+        
+
+
+        /*  [OperationContract]
+          User Login(string loginType, string password);
+
+          [OperationContract]
+          bool Register(User user);
+
+          [OperationContract]
+          List<Product> GetProducts();
+
+          [OperationContract]
+          List<Product> GetProductsOnSale();
+
+          [OperationContract]
+          bool productExists(string name);
+
+          [OperationContract]
+          void addProduct(Product product);
+
+          [OperationContract]
+          bool deleteProduct(Product product);
+
+          [OperationContract]
+          bool updateProduct(Product product);
+
+          [OperationContract]
+          List<Product> GetProductsbyCategory(string category);
+          [OperationContract]
+          List<string> GetCategories();
+
+          [OperationContract]
+          Product GetProductbyName(string prod_name);
+
+          [OperationContract]
+          void InsertToCart(int session, int productId, int qty);
+
+          [OperationContract]
+          List<Product> GetCartItems(int session);
+
+          [OperationContract]
+          void RemoveFromCart(int session, int productId);
+
+          [OperationContract]
+          Cart GetCart(int session);
+
+          [OperationContract]
+          void UpdateCart(Cart cart);
+
+          [OperationContract]
+          int PlaceOrder(Order order);
+
+          [OperationContract]
+          void GenerateInvoice(Invoice invoice);
+
+          [OperationContract]
+          List<Invoice> GetInvoicesByUserID(int Id);
+
+          [OperationContract]
+          List<Invoice> GetSortedInvoices(int userId, string orderBy);*/
+    }
+}
---
+++ GroupProject_Service/WebService.svc.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GroupProject_Service
+{
+   
+    public class WebService : IWebService
+    {
+
+        LinqDataContext dc = new LinqDataContext();
+
+        public bool AddProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToCart(int productId, int cartID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToWishList(int productId, int wishListId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool decreaseQuantity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateInvoice(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Cart GetCart(int session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetCartItems(int session)
+        {
+            throw new NotImplementedException();
+        }
+        public List<string> GetCategories()
+        {
+           
+            List<string> categories = (from p in dc.Products select p.Category).Distinct().ToList();
+
+            if (categories.Any()) 
+            {
+                return categories;
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+
+
+
+        public List<Invoice> GetInvoicesByUserID(int Id)
+        {
+              throw new NotImplementedException();
+        }
+
+        public Product GetProductByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductbyName(string prod_name)
+        {
+                throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            dynamic prodList = (from p in dc.Products select p).ToList();
+
+            if (prodList != null)
+            {
+                List<Product> toReturn = new List<Product>();
+                foreach (Product p in prodList)
+                {
+                    toReturn.Add(p);
+
+                }
+                return toReturn;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public List<Product> GetProductsbyCategory(string category)
+        {
+            dynamic prodList = (from p in dc.Products where p.Category == category select p).DefaultIfEmpty().ToList();
+            if (prodList != null)
+            {
+                return prodList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Product> GetProductsOnSale()
+        {
+            dynamic prodList = (from p in dc.Products where p.onSale == true select p).DefaultIfEmpty().ToList();
+            if (prodList != null)
+            {
+                return prodList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Product> GetProductsOnSaleOnCategory(string category)
+        {
+            dynamic prodList = (from p in dc.Products where (p.onSale == true && p.Category == category) select p).DefaultIfEmpty().ToList();
+            if (prodList != null)
+            {
+                return prodList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Product> GetFeaturedProducts()
+        {
+            dynamic prodList = (from p in dc.Products where  p.isNew == true select p).DefaultIfEmpty().ToList();
+            if (prodList != null)
+            {
+                return prodList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Invoice> GetSortedInvoices(int userId, string orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetWishListItems(int wishlistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool increaseQuantity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public User Login(string loginType, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool productExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(int session, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFromCart(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFromWishlist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCart(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool updateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool userExistence(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
